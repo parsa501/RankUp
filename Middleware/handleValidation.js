dd6547dcc0151df077c451ad9222f935608229c8
parsa501 <safazadehparsa@gmail.com>
2025-09-24 14:24:25 -0500

    add handel valition

---
+++ Middleware/handleValidation.js
@@ -0,0 +1,12 @@
+import { validationResult } from "express-validator";
+
+export default function handleValidation(req, res, next) {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      success: false,
+      errors: errors.array()
+    });
+  }
+  next();
+}
